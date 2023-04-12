46eca4d843cff05a0192018d87509b5104715cb0
penielpam <penielpam@gmail.com>
2023-04-12 13:28:32 +0000

    some changes

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,9 +6,6 @@ function that creates an Object from a “JSON file”
 import json
 
 
-def load_from_json_file(filename):
-    if filename[-4::] != 'json':
-        return "Not a JSON file"
-    """crate and object from a "JSON file" """
+def load_from_json_file(filename: str):
     with open(filename, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
