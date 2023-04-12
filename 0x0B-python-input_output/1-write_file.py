92e104322592f86a20614ffceae779bb626eb60b
penielpam <penielpam@gmail.com>
2023-04-12 12:43:58 +0000

    mandatory task

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def write_file(filename="", text=""):
-    # Open the file in write mode using 'with' statement
-    with open(filename, "w", encoding="utf-8") as file:
-        # Write the text to the file and get the number of characters written
-        num_chars_written = file.write(text)
-    # Return the number of characters written
-    return num_chars_written
+"""
+this has the function "wrtie_file"
+"""
 
+
+def write_file(filename="", text=""):
+    """returns the number of chars written to "filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
