91da82d3240016bb48935e960f8623ea78c67a03
penielpam <penielpam@gmail.com>
2023-04-12 12:03:35 +0000

    made changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,8 +4,8 @@ Contains the read_file function that is to be excicuted
 """
 
 
-def read_file(filename="me"):
+def read_file(filename=""):
     """""reads a text file(UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end="me")
+        print(f.read(), end="")
     read_file()    
