60e99e04e1ee8e8d0392dd9bf86c0d3c65cec868
penielpam <penielpam@gmail.com>
2023-04-12 12:31:22 +0000

    made changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+"""
+Contains the read_file function to excecutted
+"""
 
+
+def read_file(filename=""):
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
