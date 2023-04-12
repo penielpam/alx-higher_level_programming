172e601edc25996d0d1f4570eeba7339ed6b6ba0
penielpam <penielpam@gmail.com>
2023-04-12 12:27:33 +0000

    made changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    try:
-        with open(filename, "r", encoding="utf-8") as file:
-            for line in file:
-                print(line, end="")
-    except OSError:
-        pass
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
+
