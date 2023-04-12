0910ef93c93ca80c5aa731174cadf27a7b15bddf
penielpam <penielpam@gmail.com>
2023-04-12 12:24:04 +0000

    made changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-def read_file(filename: str = "") -> None:
-    """
-    Reads the contents of a UTF-8 encoded text file and outputs it to stdout.
-
-    Args:
-        filename (str): The name of the file to be read.
-
-    Returns:
-        None
-    """
-    with open(filename, "r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+def read_file(filename=""):
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            for line in file:
+                print(line, end="")
+    except OSError:
+        pass
