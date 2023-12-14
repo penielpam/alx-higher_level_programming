7bfce1f2ebc039fd855ea46d708a42c25f471154
penielpam <penielpam@gmail.com>
2023-12-14 07:19:13 -0800

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- show all the available shows contained in the database hbtn_0d_tvshows
+-- show all the available rows of tables in a database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
